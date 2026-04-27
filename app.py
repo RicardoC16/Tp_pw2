c136652c8c0cb9eae07cebe9241df8c7692f58cd
RicardoContreras <ricardo_rc89@hotmail.com>
2026-04-27 14:48:24 -0300

    Agrego documentacion de la api con swagger

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
+from flasgger import Swagger
 
 app = Flask(__name__)
+swagger = Swagger(app)
 
 productos = [
     {"id": 1,
@@ -32,10 +34,47 @@ carrito = [
 
 @app.route('/productos', methods = ["GET"])
 def mostrar_productos():
+    """
+    Retorna la lista de productos disponibles
+    ---
+    responses:
+      200:
+        description: Lista de productos obtenida correctamente
+        examples:
+          application/json:
+            [
+              {
+                "id": 1,
+                "nombre": "Curso de aleman inicial",
+                "precio": 1500,
+                "moneda": "Euro"
+              },
+              {
+                "id": 2,
+                "nombre": "Curso de ruso intermedio",
+                "precio": 2500,
+                "moneda": "Rublo"
+              }
+            ]
+    """
     return jsonify(productos)
 
 @app.route('/carrito', methods=["GET"])
 def mostrar_carrito():
+    """
+    Muestra el carrito con el total calculado
+    ---
+    responses:
+        200:
+            description: "muestra productos del carrito con el total"
+            examples:
+                application/json:
+                    carrito:
+                        - id: 2
+                          cantidad: 3
+                    total: 4500
+              
+    """
     total = 0
     for prod_carrito in carrito:
         for prod_lista in productos:
@@ -46,6 +85,33 @@ def mostrar_carrito():
 
 @app.route('/carrito', methods = ["POST"])
 def agregar_producto():
+    """
+    Agrega un producto al carrito
+    ---
+    parameters:
+        - name: datos_prod
+          in: body
+          required: true
+          schema:
+            type: object
+            properties:
+                id: 
+                    type: integer
+                cantidad:
+                    type: integer
+    responses:
+        200:
+            description: Muestra un mensaje de exito al agregar un producto al carrito
+            examples:
+                application/json:
+                    Mensaje: "Producto añadido existosamente"
+        404:
+            description: Muestra un mensaje de error cuando se intenta agregar un id que no existe
+            examples:
+                application/json:
+                    Error: "Id inexistente"
+
+    """
     datos_prod = request.get_json()
     id = datos_prod["id"]
     cantidad = datos_prod["cantidad"]
@@ -67,6 +133,33 @@ def agregar_producto():
 
 @app.route('/carrito', methods = ["DELETE"])
 def eliminar_producto():
+
+    """
+    Elimina un producto del carrito
+    ---
+    parameters:
+        - name: datos_prod
+          in: body
+          required: true
+          schema:
+            type: object
+            properties:
+                id: 
+                    type: integer
+    responses:
+        200:
+            description: Muestra un mensaje de exito al eliminar un producto del carrito
+            examples:
+                application/json:
+                    Mensaje: "Producto eliminado exitosamente"
+        404:
+            description: Muestra un mensaje de error cuando se intenta eliminar un id que no existe
+            examples:
+                application/json:
+                    Error: "Id inexistente"
+
+    """
+
     datos_prod = request.get_json()
     id = datos_prod["id"]
     id_a_eliminar = False
@@ -77,7 +170,7 @@ def eliminar_producto():
     if id_a_eliminar == False:
         return jsonify({"Error": "Id inexistente"}), 404
     
-    return jsonify({"Mensaje": "Producto eliminado existosamente"}), 200
+    return jsonify({"Mensaje": "Producto eliminado exitosamente"}), 200
             
 
 if __name__ == "__main__":
