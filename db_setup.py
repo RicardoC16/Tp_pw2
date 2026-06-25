e4e434695d3ff4eae38cf00793928d7383ee7728
RicardoContreras <ricardo_rc89@hotmail.com>
2026-06-25 17:11:15 -0300

    se modifican comentarios

--- db_setup.py
+++ db_setup.py
@@ -24,14 +24,12 @@ val = [
   ('Italiano inicial', 'it', 1200,'€'),
   ('Español intermedio', 'es', 9500,'€')
 ]
-#Antes de insertar, vaciar la tabla, para evitar que cada vez que ejecute se vuelvan a insertar los VAL
-#Como en carrito referencio un id en productos, si quiero vaciar productos con datos en carrito, mysql va a tirar error. Para evitarlo vaciar primero carrito y después productos
+#Se vacian las tablas productos y carritos para que cada vez que se ejecute el archivo, no se dupliquen los datos
 mycursor.execute('DELETE from carrito')
 mycursor.execute('DELETE from productos')
-mycursor.execute('ALTER TABLE productos AUTO_INCREMENT = 1')
+mycursor.execute('ALTER TABLE productos AUTO_INCREMENT = 1')#Resetear Id's
 mycursor.executemany(sql, val)
 mydb.commit()
 
-mycursor.execute('SELECT * FROM productos')
-print(list(mycursor))
+
 
